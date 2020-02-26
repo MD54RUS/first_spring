6aa25ff1af5366b3ed6d19919ad8032ab0ca8d22
Pro-rock <pro-rock@ngs.ru>
2020-02-26 15:23:23 +0700

    working API found/

--- src/main/java/com/example/first_spring/weather/RestWeatherRetriever.java
+++ src/main/java/com/example/first_spring/weather/RestWeatherRetriever.java
@@ -1,7 +1,5 @@
 package com.example.first_spring.weather;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -12,7 +10,9 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class RestWeatherRetriever implements WeatherRetriever {
     private final RestTemplate restTemplate;
+    //http://api.weatherapi.com/v1/forecast.json?key=3b84fcc031024ddc84a71129202602&q=Novosibirsk&days=1
     private static java.net.URI URI = java.net.URI.create("https://community-open-weather-map.p.rapidapi.com/weather");
+    private static String API_URL = "http://api.weatherapi.com/v1/forecast.json?key=";
     MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 
 
@@ -22,10 +22,11 @@ public class RestWeatherRetriever implements WeatherRetriever {
 
     @Override
     public String getForecast() {
-        headers.add("x-rapidapi-host","community-open-weather-map.p.rapidapi.com");
-        headers.add("x-rapidapi-key","5ca16e2f72msh712306e3d9e9d42p1949f4jsn3de39cad8c90");
-        RequestEntity requestEntity = new RequestEntity(headers, HttpMethod.GET, URI);
-        ResponseEntity<String> result = restTemplate.exchange(requestEntity, String.class);
+//        headers.add("x-rapidapi-host","community-open-weather-map.p.rapidapi.com");
+//        headers.add("x-rapidapi-key","5ca16e2f72msh712306e3d9e9d42p1949f4jsn3de39cad8c90");
+//        RequestEntity requestEntity = new RequestEntity(headers, HttpMethod.GET, URI);
+//        ResponseEntity<String> result = restTemplate.exchange(requestEntity, String.class);
+        ResponseEntity<String> result = restTemplate.getForEntity(API_URL + GetApiKey.getKey() + "&q=Novosibirsk&days=1", String.class);
         return result.getBody();
     }
 }
